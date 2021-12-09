dad29061fe74502b225312d67a20b5797c433920
Felipe Silva <felipe726silva@gmail.com>
2021-12-09 17:49:26 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import About from './components/about';
-import Navbar from './components/header';
-import Home from './components/main';
-import Skills from './components/skills';
-import Projects from './components/projects';
-import Contact from './components/contact';
-import Footer from './components/footer';
+import About from './components/about/index.jsx';
+import Navbar from './components/header/index.jsx';
+import Home from './components/main/index.jsx';
+import Skills from './components/skills/index.jsx';
+import Projects from './components/projects/index.jsx';
+import Contact from './components/contact/index.jsx';
+import Footer from './components/footer/index.jsx';
 
 const App = () => {
   return (
