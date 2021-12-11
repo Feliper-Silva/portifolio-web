b50dd4abc9b306476a52e35b0529b9213250743b
Felipe Silva <felipe726silva@gmail.com>
2021-12-10 21:21:07 -0300

    terceiro commit

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -5,45 +5,43 @@ function Projects() {
     <section className="projects" id="projects">
       <div className="max-width">
         <h2 className="title">Projetos</h2>
-        <div className="carousel owl-carousel">
-          <div className="card">
-            <div className="box">
-              <video loop autoPlay muted>
-                <source src="/video/project-math.mp4" type="video/mp4"></source>
-                Your browser not support the video tag.
-              </video>
-              <div className="text">Math question</div>
-              <p>
-                Um jogo de questões matemática, feito com HTML5, CSS e
-                JavaScript + JQuery.
-              </p>
-            </div>
+        <div className="card">
+          <div className="box">
+            <video loop autoPlay muted>
+              <source src="/video/project-math.mp4" type="video/mp4"></source>
+              Your browser not support the video tag.
+            </video>
+            <div className="text">Math question</div>
+            <p>
+              Um jogo de questões matemática, feito com HTML5, CSS e JavaScript
+              + JQuery.
+            </p>
           </div>
-          <div className="card">
-            <div className="box">
-              <video loop autoPlay muted>
-                <source src="/video/project-math.mp4" type="video/mp4"></source>
-                Your browser not support the video tag.
-              </video>
-              <div className="text">Math question</div>
-              <p>
-                Um jogo de questões matemática, feito com HTML5, CSS e
-                JavaScript + JQuery.
-              </p>
-            </div>
+        </div>
+        <div className="card">
+          <div className="box">
+            <video loop autoPlay muted>
+              <source src="/video/project-math.mp4" type="video/mp4"></source>
+              Your browser not support the video tag.
+            </video>
+            <div className="text">Math question</div>
+            <p>
+              Um jogo de questões matemática, feito com HTML5, CSS e JavaScript
+              + JQuery.
+            </p>
           </div>
-          <div className="card">
-            <div className="box">
-              <video loop autoPlay muted>
-                <source src="/video/project-math.mp4" type="video/mp4"></source>
-                Your browser not support the video tag.
-              </video>
-              <div className="text">Math question</div>
-              <p>
-                Um jogo de questões matemática, feito com HTML5, CSS e
-                JavaScript + JQuery.
-              </p>
-            </div>
+        </div>
+        <div className="card">
+          <div className="box">
+            <video loop autoPlay muted>
+              <source src="/video/project-math.mp4" type="video/mp4"></source>
+              Your browser not support the video tag.
+            </video>
+            <div className="text">Math question</div>
+            <p>
+              Um jogo de questões matemática, feito com HTML5, CSS e JavaScript
+              + JQuery.
+            </p>
           </div>
         </div>
       </div>
